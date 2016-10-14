26eb84848881f170cc5c9abf957411dfd3349ad9
user <user@FA10>
2016-10-14 09:53:38 +0800

    HW3

---
+++ src/fcu/iecs/oop/Break.java
@@ -0,0 +1,14 @@
+package fcu.iecs.oop;
+
+public class Break {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; i ++) {
+			System.out.println(i);
+			if(i==1)
+				break;
+			}
+
+	}
+
+}
---
+++ src/fcu/iecs/oop/Continue.java
@@ -0,0 +1,18 @@
+package fcu.iecs.oop;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; i ++) {
+			if(i==0)
+				continue;
+			if(i==2)
+				continue;
+			if(i==4)
+				continue;
+			System.out.println(i);
+			}
+
+	}
+
+}
