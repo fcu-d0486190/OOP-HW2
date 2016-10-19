5edef43cc4079730dd7dea7509e7f433f5294adb
fcu-d0486190 <dfg78951236@gmail.com>
2016-10-19 22:52:07 +0800

    Update StringComparator.java
    HW3

--- src/fcu/iecs/oop/StringComparator.java
+++ src/fcu/iecs/oop/StringComparator.java
@@ -1,20 +1,23 @@
 package fcu.iecs.oop;
+
 import java.util.Scanner;
+
 public class StringComparator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Enter a string 1:");
-		String st1 = keyboard.nextLine();
-		System.out.println("Enter a string 2:");
-		String st2 = keyboard.nextLine();
-		st1 = st1.toUpperCase();
-		st2 = st2.toUpperCase();
-		if(st1==st2)
+		while(true){
+			System.out.println("Enter a string 1:");
+			String st1 = keyboard.nextLine();
+			System.out.println("Enter a string 2:");
+			String st2 = keyboard.nextLine();
+			st1 = st1.toUpperCase();
+			st2 = st2.toUpperCase();
+			if(st1.equals(st2))
+				break;
+		}
 			System.out.println("The two strings are the same.");
-		else
-			System.out.println("The two strings are not the same.");
 
 	}
 
